23eb0def4a188536a21547ca7aad119748c60de8
MKLab <MKLab@Launchcode8.stlcc.local>
2021-11-25 22:53:17 -0600

    calibrated with pearce's video, ready to start

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //use:  preventDefault()
 window.addEventListener("load", function() {
 
-   let listedPlanets;
+ /*  let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
    listedPlanetsResponse.then(function (result) {
@@ -11,6 +11,24 @@ window.addEventListener("load", function() {
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   // should be back to normal
+   })   */
+
+//22:40 comments on adding validation: are already inside of a window loadevent listener^^^
+//bellow selects form and listens for the submit  button event
+let form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    //validate 4 form inputs with dom and conditionals
+   //call form submission function to do that
+//   define variables with element values on this page
+   
+   
+   
+    alert("submit clicked");
+
 });
+   
+
+
+});
+
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,14 +17,35 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+   // this function can be considered unessasary
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
-}
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {  //call function in script.js
+//    first two are the document paramater as is and the faultyItems div is what list is
+   //part 2 (validation) a. every field needs an entry or send alert. b.  strings for names and numbers for fuel and cargo lvl 
+   //part 3 (update ready n not listis [claimed to be literally most of the code]) 
+   //& part 4 too maybe (indicate list changes and ready status on the page and css with dom)
+   //build with varriables passed in
+   //big if/elseif
 
-async function myFetch() {
+
+   //c1 error condition: if any value is empty, send simple alert instead. say: "ALL Fields Required"
+   alert("All Fields Required"); 
+   //select html elemnts first
+}//c2 error condition: make sure the data is string or number, use is NaN, send better alert if incorrect
+//c3 elseif sofarsogood, assume validation is correct and process comparisons
+//since everyting else on is correct conditions, embed in c3
+//what if fuel level and cargo are not in range (both <10k, both>10k ,fl>10k,c>10k)
+// in any of this, list param faulty items becomes visible. with the dom
+// pilot and copilot status text should be updated with a template literal.
+//update the html elements on the page, how? seek book and html tags probably
+
+//before launchstatus besaying ready for launch, what does it mean to be ready fir launch? enough fuel, low enough cargo mass basicaly
+
+//update color too
+
+
+async function myFetch() {   //part 5 last part, somewhat seperate
     let planetsReturned;
 
     planetsReturned = await fetch().then( function(response) {
