38c9d0dac0a8f61ec7e13958aecda5945b45662d
MKLab <MKLab@Launchcode8.stlcc.local>
2021-11-27 02:09:22 -0600

    Part 4 finished, everything on the page up to the fetch json part is working correctly!

--- scriptHelper.js
+++ scriptHelper.js
@@ -18,7 +18,6 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 function validateInput(testInput) {/*this function can be considered unessasary*/ console.log("unused function called");}
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-//first two are the document paramater as is and the faultyItems div is what list is
     let bananAlert = "Pilot and Copilot feilds need to be names and Fuel Level and Cargo Mass fields need to be Numbers";
     let afr = "ALL Fields Required!"; //1 use
     let fuelMin = 10000;
@@ -26,29 +25,37 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     let fuelReady= false;
     let cargoReady = false; 
     let readyForLaunch =false;
+    let launchStatus = document.getElementById("launchStatus");
 if (/*any value is empty*/pilot===""||copilot===""||fuelLevel===""||cargoLevel===""){alert(afr);}
 else if((!isNaN(pilot))||(!isNaN(copilot))||isNaN(fuelLevel)||isNaN(cargoLevel)){alert(bananAlert);}
 else{if (fuelLevel>fuelMin){fuelReady = true;}
 if (cargoLevel<cargoMax){cargoReady = true;}
-alert("update lines 1 and 2 with name template literals to start");  //update part 4
+document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`;
+document.getElementById("copilotStatus").innerHTML = `Copilot ${copilot} is ready for launch`;
 if (cargoReady === false && fuelReady === false){
-    alert("update line3&4");          //update in part 4
-    readyForLaunch = false;
+      document.getElementById("cargoStatus").innerHTML = "Cargo mass too heavy for launch";
+     document.getElementById("fuelStatus").innerHTML = "Fuel level too low for launch";
+     readyForLaunch = false;
 } else if(cargoReady === false && fuelReady === true){
-    alert("update line4");          //update in part 4
+    document.getElementById("cargoStatus").innerHTML = "Cargo mass too heavy for launch";
+    document.getElementById("fuelStatus").innerHTML = "Fuel level high enough for launch";
     readyForLaunch = false;
 }else if(fuelReady === false && cargoReady === true){
-alert("update line3");     //update in part 4
+    document.getElementById("fuelStatus").innerHTML = "Fuel level too low for launch";
+    document.getElementById("cargoStatus").innerHTML = "Cargo mass is low enough for launch";
 readyForLaunch = false;
 }else{
+    document.getElementById("fuelStatus").innerHTML = "Fuel level high enough for launch";
+    document.getElementById("cargoStatus").innerHTML = "Cargo mass is low enough for launch";
   readyForLaunch = true;
     alert("ready!");
 }
-if (readyForLaunch)
-{
-    // show ready in green
+if (readyForLaunch){
+    document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch";
+    launchStatus.style.color = "green";
 }else{
-    //show not ready in red
+    document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+   launchStatus.style.color = "red";
 }
     list.style.visibility="visible";
 }
